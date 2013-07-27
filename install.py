032418cfb02067b8efe0fd7fc96048a18537c2d7
volodja <volodja@mail.com>
2013-07-28 04:18:24 +0700

    rotramfs scripts

---
+++ install.py
@@ -0,0 +1,328 @@
+#!/usr/bin/python3
+# coding=utf8
+
+
+__author__ = 'volodja'
+
+
+import os
+import re
+import sys
+import atexit
+
+
+is_need_restore         = False
+file_name_fstab         = "/etc/fstab"
+file_name_fstab_backup  = "/etc/fstab.rootramfsbackup"
+file_name_hook          = "/usr/share/initramfs-tools/hooks/rootramfshook"
+file_name_loader        = "/scripts/loader"
+
+
+def printList(name, list_):
+    print(name + ":")
+    for row in list_:
+        print(str(row).replace("\n", ""))
+
+
+# выполняет шелл команду
+def executeShellCommand(command):
+    print("exec: " + command)
+    result_pipe = os.popen(command)
+    result_     = result_pipe.readlines()
+    result      = []
+    for line in result_:
+        result.append(line.replace('\n', ''))
+    return result
+
+
+# читает /etc/fstab в 2хмерный массив(return) пропуская строки "#..."
+# @return:    fstab table
+def readFSTab():
+    print("read " + file_name_fstab + " ...")
+    file_fstab      = open(file_name_fstab)
+    fstab_text      = file_fstab.readlines()
+    fstab           = []
+    file_fstab.close()
+
+    for line in fstab_text:
+        if line[0] != "#":
+            fstab_line = re.split(r"\s+", line.replace("\n", "").replace("UUID=", ""))
+            fstab.append(fstab_line)
+
+    #printList("fstab", fstab)
+    print("read " + file_name_fstab + " OK")
+    return fstab
+
+
+# читает вывод blkid и возвращает в виде двухмерного массива
+# @return:    blkid table
+def readBlkID():
+    print("read blkid devices ...")
+    blkid_result    = executeShellCommand("blkid")
+    blkid           = []
+    for line in blkid_result:
+        row = line.replace(":", "").replace("\"", "").replace("UUID=", "").replace("TYPE=", "") .split(" ")
+        row.remove('')
+        blkid.append(row)
+
+    if len(blkid) == 0:
+        print("error: empty blkid result")
+        exit(1)
+
+    #printList("blkid", blkid)
+    print("read blkid devices OK")
+    return blkid
+
+
+# выбирает валидные строки из таблицы fstab соответствующие стандартным существующим точкам монтирования
+# если не найденаточка монтирования "/boot" на отдельном устройстве, то завершается работа скрипта 
+def selectSystemFSTab(fstab):
+    print("search system partitions ...")
+    fstab_system    = ["/", "/boot", "/home", "/tmp", "/usr", "/var", "/srv", "/opt", "/usr/local"]
+    fstab_select    = []
+    is_found_boot   = False
+    
+    for row_fstab in fstab:
+        if len(row_fstab) < 6:
+            continue
+        if row_fstab[1] == "/boot":
+            is_found_boot = True
+            continue
+        for  row_fstab_system in fstab_system:
+            if row_fstab[1] == row_fstab_system:
+                fstab_select.append(row_fstab)
+
+    #printList("fstab_select", fstab_select)
+    
+    if is_found_boot:
+        print("found separate boot mount point, OK")
+    else:
+        print("boot mount point not found, need separate boot mount point")
+        exit(1) #----->
+    
+    print("search system partitions OK")
+    return fstab_select
+
+
+# конвертирует названия устройств в fstab в UUID формат
+# @return: fstab UUID format
+def convertFSTabToUUIDFSTab(fstab):
+    print("convert fstab devices to UUID ...")
+    blkid       = readBlkID()
+    fstab_uuid  = []
+
+    for row_fstab in fstab:
+        for row_blkid in blkid:
+            if  row_fstab[0] == row_blkid[0]:
+                row_fstab[0] =  row_blkid[1]
+                break
+        fstab_uuid.append(row_fstab)
+
+    #printList("fstab_uuid", fstab_uuid)
+    print("convert fstab devices to UUID OK")
+    return fstab_uuid
+
+
+# добавляет к таблице fstab колонку слева с путями устройств /dev/ram[0..]
+# @return: fstab_ram
+def addRAMDevicesColumnToFSTab(fstab):
+    print("add ram devices to fstab ...")
+    fstab_ram   = []
+    i           = 0
+    for row_fstab in fstab:
+        row_fstab.append("/dev/ram" + str(i))
+        i = i + 1
+        fstab_ram.append(row_fstab)
+
+    #printList("fstab_ram", fstab_ram)
+    print("add ram devices to fstab OK")
+    return fstab_ram
+
+
+# добавляет к таблице fstab колонку справа с размерами устройств /dev/ram[0..]
+# @return: fstab_size
+def addDevicesSizeColumnToFSTab(fstab):
+    print("calculate partitions size ...")
+    fstab_size  = []
+    for row_fstab in fstab:
+        size = executeShellCommand("blockdev --getsize64 /dev/disk/by-uuid/" + row_fstab[0])[0]
+        if len(size) == 0:
+            print("blockdev --getsize64 /dev/disk/by-uuid/" + row_fstab[0] + " return Null")
+            exit(1)
+        row_fstab.append(size)
+        fstab_size.append(row_fstab)
+
+    #printList("fstab_size", fstab_size)
+    print("calculate partitions size OK")
+    return fstab_size
+
+
+# создает hook-обработчик, выполняемый при изменении initrd
+def createHook(fstab):
+    print("create " + file_name_hook + " ...")
+    rootramfshook   = file_name_hook
+    file_hook       = open(rootramfshook, "wt")
+    loader_text     = []
+    size            = 16 * 1024 * 1024
+    i               = 1
+
+    for row_fstab in fstab:
+        size = size + int(row_fstab[6])
+
+    print("required ram size: " + str(size) + " bytes")
+    ram_total_text  = executeShellCommand("cat /proc/meminfo")
+    ram_total       = 0
+
+    for row_ram_total_text in ram_total_text:
+        if row_ram_total_text[0:9] == "MemTotal:":
+            ram_total = int(row_ram_total_text.replace("MemTotal:", "").replace("kB", "").replace(" ", ""))
+
+    if ram_total == 0:
+        print("error read MemTotal")
+        exit(1)
+
+    if ram_total * 1024 < size + 512 * 1024 * 1024:
+        print("not enough memory, need to add another " + str((size + 512 * 1024 * 1024) - ram_total * 1024) + " bytes")
+        exit(1)
+
+    #создание скрипта initrd:/scripts/loader загрузки разделов в оперативку
+    loader_text.append("mkdir -p /ram &&")
+    loader_text.append("mount -t tmpfs -o size=" + str(size) + " none /ram &&")
+
+    for row_fstab in fstab:
+        device  = row_fstab[0].replace("UUID=", "")
+        loader_text.append("echo 'load " + str(i) + " of " + str(len(fstab)) + " block device " + device + "' &&")
+        loader_text.append("dd if=/dev/disk/by-uuid/" + device + " of=/ram/" + device + " bs=8388608 &&")
+        i       = i + 1
+
+    for row_fstab in fstab:
+        if row_fstab[1] == "/":
+            loader_text.append("mount -t " + row_fstab[2] + " -o loop /ram/" + row_fstab[0] + " /root &&")
+            loader_text.append("mkdir -p /root/ram &&")
+            loader_text.append("mount -o bind /ram /root/ram &&")
+
+    loader_text.append("mount -o remount -o ro /root &&")
+    loader_text.append("echo 'done' && sleep 1 && exit 0")
+    loader_text.append("echo 'fail' && exit 1")
+    
+    #создание скрипта initrd-hook, /usr/share/initramfs-tools/hooks/rootramfshook
+    #и вставка в него кода для создания скрипта initrd:/scripts/loader
+    hook_text = []
+    hook_text.append('echo "\\n\\' + "\n")
+    for row_loader in loader_text:
+        hook_text.append(row_loader + '\\n\\' + "\n")
+        
+    hook_text.append('\n" >    $DESTDIR"' + file_name_loader + '" && ')   # + "\n"
+    hook_text.append('chmod +x $DESTDIR"' + file_name_loader + '" && \n') # + "\n"
+    
+    hook_text.append(
+''' echo \'74c74,75
+< \tmount ${roflag} ${FSTYPE:+-t ${FSTYPE} }${ROOTFLAGS} ${ROOT} ${rootmnt}
+---
+> \t#mount ${roflag} ${FSTYPE:+-t ${FSTYPE} }${ROOTFLAGS} ${ROOT} ${rootmnt}
+> \t/scripts/loader
+' | patch -sf $DESTDIR"/scripts/local" || echo "fail patch"
+''')
+        
+    file_hook.writelines(hook_text)
+    
+    executeShellCommand("chmod +x " + rootramfshook)
+    print("create " + file_name_hook + " OK")
+
+
+def patchFSTab(fstab):
+    print("patch " + file_name_fstab + " ...")
+    if not os.path.isfile(file_name_fstab_backup):
+        print("backup " + file_name_fstab + " to " + file_name_fstab_backup)
+        cp = executeShellCommand("cp " + file_name_fstab + " " + file_name_fstab_backup)
+        if len(cp) > 0:
+            print("error backup " + file_name_fstab + ": " + str(cp))
+
+    file_fstab      = open(file_name_fstab)
+    fstab_text      = file_fstab.readlines()
+    fstab_text_ram  = []
+
+    file_fstab.close()
+
+    for row_fstab_text in fstab_text:
+        is_found = False
+        for row_fstab in fstab:
+            if re.split(r"\s+", row_fstab_text)[1] == row_fstab[1]  and  row_fstab_text[0] != "#":
+                is_found = True
+
+                if is_found:
+                    fstab_text_ram.append("#" + row_fstab_text)
+                    fstab_text_ram.append("/ram/" + row_fstab[0] + "\t" + row_fstab[1] + "\t" + row_fstab[2] + "\t" + row_fstab[3] + ",loop\t0\n")
+                    break
+
+        if not is_found:
+            fstab_text_ram.append(row_fstab_text)
+
+    fstab_text_ram.append("\n")
+    file_fstab = open(file_name_fstab, "wt")
+    file_fstab.writelines(fstab_text_ram)
+    print("patch " + file_name_fstab + " OK")
+    #printList("fstab_text_ram", fstab_text_ram)
+
+
+def restore():
+    #TODO: restore over patch fstab instead restore backup
+    print("restore ...")
+    if os.path.isfile(file_name_fstab_backup):
+        print("restore  " + file_name_fstab_backup + " to " + file_name_fstab)
+        mv = executeShellCommand("mv " + file_name_fstab_backup + " " + file_name_fstab)
+        if len(mv) > 0:
+            print("error restore " + file_name_fstab + ": " + str(mv))
+        else:
+            executeShellCommand("/usr/bin/rootramfs --sync /etc/fstab")
+
+    if os.path.isfile(file_name_hook):
+        print("remove " + file_name_hook)
+        rm = executeShellCommand("rm " + file_name_hook)
+        if len(rm) > 0:
+            print("error remove " + file_name_hook + ": " + str(mv))
+
+    print("restore OK")
+
+
+def onExit():
+    if is_need_restore:
+        restore()
+    else:
+        print("all done")
+
+    printList("update-initramfs -u", executeShellCommand("update-initramfs -u"))
+
+
+if len(sys.argv) == 3:
+    dpkg_script     = sys.argv[1]
+    dpkg_command    = sys.argv[2]
+    print("scritp:  " + dpkg_script)
+    print("command: " + dpkg_command)
+
+    if dpkg_script == "postinst":
+        atexit.register(onExit)
+        if dpkg_command == "configure":
+            is_need_restore = True
+            fstab = readFSTab()
+            fstab = selectSystemFSTab(fstab)
+            fstab = convertFSTabToUUIDFSTab(fstab)
+            fstab = addDevicesSizeColumnToFSTab(fstab)
+
+            createHook(fstab)
+            patchFSTab(fstab)
+            is_need_restore = False
+
+        if dpkg_command == "abort-upgrade"  or  dpkg_command == "abort-remove"  or  dpkg_command == "abort-deconfigure":
+            is_need_restore = True
+
+    if dpkg_script == "prerm":
+        atexit.register(onExit)
+#        if dpkg_command == "remove"  or  
+#            dpkg_command == "purge"  or  
+#            dpkg_command == "upgrade"  or  
+#            dpkg_command == "failed-upgrade"  or  
+#            dpkg_command == "abort-install"  or  
+#            dpkg_command == "abort-upgrade"  or  
+#            dpkg_command == "disappear":
+        is_need_restore = True
---
+++ rootramfs.py
@@ -0,0 +1,173 @@
+#!/usr/bin/python
+
+
+__author__ = 'volodja'
+
+
+import os
+import re
+import sys
+#import getpass
+
+
+def printList(name, list_):
+    print name + ":"
+    for row in list_:
+        print str(row).replace("\n", "")
+
+
+def executeShellCommand(command):
+    print "exec: " + command
+    result_pipe = os.popen(command)
+    result_     = result_pipe.readlines()
+    result      = []
+    for line in result_:
+        result.append(line.replace('\n', ''))
+    return result
+
+
+def readFSTab():
+    file_fstab      = open("/etc/fstab")
+    #file_fstab      = open("/etc/fstab.1")
+    fstab_text      = file_fstab.readlines()
+    fstab           = []
+    file_fstab.close()
+
+    for line in fstab_text:
+        if line[0] != "#":
+            fstab_line = re.split(r"\s+", line.replace("\n", "").replace("UUID=", ""))
+            fstab.append(fstab_line)
+
+    #printList("fstab", fstab)
+    return fstab
+
+
+def mountSyncFSTab():
+    fstab           = readFSTab()
+    mount           = executeShellCommand("mount")
+
+    #print "fstab: " + str(fstab)
+    #print("mount: " + str(mount))
+
+    fstab_to_mount  = []
+    for row_fstab in fstab:
+        if len(row_fstab) < 2:
+            continue
+        is_need_mount   = True
+        if row_fstab[1] == "/":
+            mount_point = "/ram/sync"
+        else:
+            mount_point = "/ram/sync" + row_fstab[1]
+        for row_mount in mount:
+            if len(row_fstab) > 1  and  (row_fstab[0][0:4] != "/ram"  or  (row_fstab[0][0:4] == "/ram"  and  row_mount.split(" ")[2] == mount_point)):
+                is_need_mount = False
+                break
+        if is_need_mount:
+            fstab_to_mount.append(row_fstab)
+
+    #print "fstab_to_mount = " + str(fstab_to_mount)
+    #exit(0)
+    for row_fstab in fstab_to_mount:
+        mount = executeShellCommand("mkdir -p /ram/sync" + row_fstab[1] + " && mount -t " + row_fstab[2] +  
+                               " /dev/disk/by-uuid/" + row_fstab[0][4:] + " /ram/sync" + row_fstab[1] + " || echo 'mount fail'")
+        if len(mount) > 0:
+            print "error: " + str(mount)
+            exit(1)
+
+
+def umountSyncFSTab():
+    #fstab           = readFSTab()
+    mount           = executeShellCommand("mount")
+    fstab_to_umount = []
+    is_was_root     = False
+
+    for row_mount in mount:
+        mount_point = str(row_mount.split(" ")[2])
+        if mount_point[0:9] == "/ram/sync":
+            if len(mount_point) > 9:
+                fstab_to_umount.append(mount_point)
+            else:
+                is_was_root = True
+    if is_was_root:
+        fstab_to_umount.append("/ram/sync")
+
+    for row_umount in fstab_to_umount:
+        umount = executeShellCommand("umount " + row_umount)
+        if len(umount) > 0:
+            print "error: " + str(umount)
+            exit(1)
+
+
+def syncCommand(args, command_, is_reverse = False):
+    paths       = []
+
+    for row_args in args:
+        paths.append(os.path.abspath(row_args))
+
+    if is_reverse:
+        dst = paths[0]
+        src = "/ram/sync" + paths[0]
+    else:
+        src = paths[0]
+        dst = "/ram/sync" + paths[0]
+
+    if os.path.isdir(paths[0]):
+        src     = src + "/"
+
+    del paths[0]
+
+    #user_name   = getpass.getuser()
+    command     = command_ + " "
+    command     = command + "--exclude '*/.gvfs' "
+    command     = command + "--exclude '/run/user/*/gvfs' "
+    command     = command + "--exclude '/media' "
+    command     = command + "--exclude '/mnt' "
+    command     = command + "--exclude '/dev' "
+    command     = command + "--exclude '/sys' "
+    command     = command + "--exclude '/proc' "
+    command     = command + "--exclude '/selinux' "
+    command     = command + "--exclude '/ram' "
+    command     = command + "--exclude '/boot' "
+
+    for row_paths in paths:
+        command = command + "--exclude '" + row_paths + "' "
+
+    command     = command + src + " " + dst + " | grep -v 'xfer#' && sync"
+    rsync       = executeShellCommand(command)
+    printList(command_, rsync)
+
+
+def syncPath(args):
+    syncCommand(args, "rsync --progress --delete -a")
+
+
+def diffPath(args):
+    syncCommand(args, "diff -r")
+
+
+def resetPath(args):
+    syncCommand(args, "rsync --progress --delete -a", True)
+
+
+if len(sys.argv) >= 3  and  sys.argv[1] == "--sync":
+    mountSyncFSTab()
+    syncPath(sys.argv[2:])
+    umountSyncFSTab()
+    exit(0)
+
+if len(sys.argv) >= 3  and  sys.argv[1] == "--reset":
+    mountSyncFSTab()
+    resetPath(sys.argv[2:])
+    umountSyncFSTab()
+    exit(0)
+
+if len(sys.argv) >= 3  and  sys.argv[1] == "--diff":
+    mountSyncFSTab()
+    diffPath(sys.argv[2:])
+    umountSyncFSTab()
+    exit(0)
+
+
+print "usage: rootramfs --sync  sync_path [exclude_path1] [exclude_path2] ..."
+print "usage: rootramfs --reset sync_path [exclude_path1] [exclude_path2] ..."
+print "usage: rootramfs --diff  sync_path [exclude_path1] [exclude_path2] ..."
